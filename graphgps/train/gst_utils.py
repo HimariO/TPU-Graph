b26841f76aeb9225f278db1b23d85ffd65fb6cb8
HimariO <dsfhe49854@gmail.com>
2023-09-25 16:20:47 +0800

    Taking some notes on baseline performance

--- graphgps/train/gst_utils.py
+++ graphgps/train/gst_utils.py
@@ -132,8 +132,11 @@ class TPUModel(torch.nn.Module):
         self.op_weights = nn.Parameter(torch.ones(1,1,requires_grad=True) * 100)
         self.config_weights = nn.Parameter(torch.ones(1, 18, requires_grad=True) * 100)
         self.history = History(500000000, 1)
-        if self.enc_config:
-            self.config_map = nn.Linear(180, 32, bias=True)
+        if enc_config:
+            self.config_map = nn.Sequential(
+                nn.Linear(180, 32, bias=True),
+                nn.BatchNorm1d(32),
+            )
         
         self.input_feat_key = input_feat_key
         self.enc_config = enc_config
