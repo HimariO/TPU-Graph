0034507f227cbf6f2ac4e120c9f9716a65ed4a4a
HimariO <dsfhe49854@gmail.com>
2023-10-26 12:50:45 +0800

    Add NerualNDCG loss, loss func config

--- graphgps/train/custom_tpu_train.py
+++ graphgps/train/custom_tpu_train.py
@@ -32,66 +32,7 @@ from graphgps.train.gst_utils import (
     TPUModel,
     CheckpointWrapper,
 )
-
-
-def listMLE(y_pred, y_true, eps=1e-7, padded_value_indicator=-1):
-    """
-    ListMLE loss introduced in "Listwise Approach to Learning to Rank - Theory and Algorithm".
-    :param y_pred: predictions from the model, shape [batch_size, slate_length]
-    :param y_true: ground truth labels, shape [batch_size, slate_length]
-    :param eps: epsilon value, used for numerical stability
-    :param padded_value_indicator: an indicator of the y_true index containing a padded item, e.g. -1
-    :return: loss value, a torch.Tensor
-    
-    ref: https://github.com/allegro/allRank/blob/master/allrank/models/losses/listMLE.py
-    """
-    # shuffle for randomised tie resolution
-    random_indices = torch.randperm(y_pred.shape[-1])
-    y_pred_shuffled = y_pred[:, random_indices]
-    y_true_shuffled = y_true[:, random_indices]
-
-    y_true_sorted, indices = y_true_shuffled.sort(descending=True, dim=-1)
-
-    mask = y_true_sorted == padded_value_indicator
-
-    preds_sorted_by_true = torch.gather(y_pred_shuffled, dim=1, index=indices)
-    preds_sorted_by_true[mask] = float("-inf")
-
-    max_pred_values, _ = preds_sorted_by_true.max(dim=1, keepdim=True)
-
-    preds_sorted_by_true_minus_max = preds_sorted_by_true - max_pred_values
-
-    cumsums = torch.cumsum(preds_sorted_by_true_minus_max.exp().flip(dims=[1]), dim=1).flip(dims=[1])
-
-    observation_loss = torch.log(cumsums + eps) - preds_sorted_by_true_minus_max
-
-    observation_loss[mask] = 0.0
-
-    return torch.mean(torch.sum(observation_loss, dim=1))
-
-
-def pairwise_hinge_loss_batch(pred, true, base_margin=0.1, adaptive=False):
-    # pred: (batch_size, num_preds )
-    # true: (batch_size, num_preds)
-    batch_size = pred.shape[0]
-    num_preds = pred.shape[1]
-    i_idx = torch.arange(num_preds).repeat(num_preds)
-    j_idx = torch.arange(num_preds).repeat_interleave(num_preds)
-
-    pairwise_true = true[:,i_idx] > true[:,j_idx]
-    if adaptive:
-        fp_true = true.float()
-        step = (fp_true.var(dim=1, keepdim=True)**0.5) * 6 / 10
-        pairwise_scale = nn.functional.relu(true[:,i_idx] - true[:,j_idx]) * pairwise_true / step
-        pairwise_scale = torch.clip(pairwise_scale, min=0, max=10)
-        margin = (pairwise_scale + 1) * base_margin
-    else:
-        margin = base_margin
-
-    loss = nn.functional.relu(margin - (pred[:,i_idx] - pred[:,j_idx]))
-    loss = loss * pairwise_true.float()
-    loss = torch.sum(loss) / batch_size
-    return loss
+from graphgps.train.custom_loss import apply_tpu_loss
 
 
 @ulogger.catch(reraise=True)
@@ -164,8 +105,7 @@ def train_epoch(logger, loader, model: TPUModel, optimizer, scheduler, emb_table
         if 'TPUGraphs' in cfg.dataset.name:
             pred = pred.view(-1, num_sample_config)
             true = true.view(-1, num_sample_config)
-            loss = pairwise_hinge_loss_batch(pred, true, adaptive=cfg.train.adap_margin)
-            # loss = listMLE(pred, true)
+            loss = apply_tpu_loss(pred, true)
             _true = true.detach().to('cpu', non_blocking=True)
             _pred = pred.detach().to('cpu', non_blocking=True)
         else:
@@ -282,7 +222,7 @@ def eval_epoch(logger, loader, model: TPUModel, split='val'):
         if 'TPUGraphs' in cfg.dataset.name:
             pred = pred.view(-1, batch_num_sample)
             true = true.view(-1, batch_num_sample)
-            loss = pairwise_hinge_loss_batch(pred, true)
+            loss = apply_tpu_loss(pred, true)
             _true = true.detach().to('cpu')
             _pred = pred.detach().to('cpu')
             
